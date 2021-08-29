41839df7a3e6ad3d2850c6a6fa2be5a638bd51b7
Drakonkinst <drakonkinstmc@gmail.com>
2021-08-28 17:02:56 -0700

    Implement top-down game

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,148 +1,221 @@
-let config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false,
-            gravity: { y: 0 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-let game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image("ship", "assets/spaceShips_001.png");
-    this.load.image("otherPlayer", "assets/enemyBlack5.png");
-    this.load.image('star', 'assets/star_gold.png');
-}
-
-function create() {
-    let self = this;
-    this.socket = io();
-    this.otherPlayers = this.physics.add.group();
-    this.socket.on("currentPlayers", players => {
-        for(id in players) {
-            if(players.hasOwnProperty(id)) {
-                if(players[id].playerId === self.socket.id) {
-                    console.log("Adding player self");
-                    addPlayer(self, players[id]);
-                } else {
-                    addOtherPlayer(self, players[id]);
-                }
-            }
-        }
-    })
-    this.socket.on("newPlayer", playerInfo => {
-        console.log("newPlayer received");
-        addOtherPlayer(self, playerInfo);
-    });
-    this.socket.on("playerDisconnect", playerId => {
-        console.log("playerDisconnect received");
-        self.otherPlayers.getChildren().forEach(otherPlayer => {
-            if(playerId === otherPlayer.playerId) {
-                otherPlayer.destroy();
-            }
-        })
-    });
-    
-    this.socket.on("playerMoved", function (playerInfo) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if(playerInfo.playerId === otherPlayer.playerId) {
-                otherPlayer.setRotation(playerInfo.rotation);
-                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-            }
-        });
-    });
-    
-    // handle player input
-    this.cursors = this.input.keyboard.createCursorKeys();
-    
-    this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
-    this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
-
-    this.socket.on('scoreUpdate', function (scores) {
-        self.blueScoreText.setText('Blue: ' + scores.blue);
-        self.redScoreText.setText('Red: ' + scores.red);
-    });
-    
-    this.socket.on('starLocation', function (starLocation) {
-        if(self.star) self.star.destroy();
-        self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
-        self.physics.add.overlap(self.ship, self.star, function () {
-            this.socket.emit('starCollected');
-        }, null, self);
-    });
-}
-
-function update() {
-    if(this.ship) {
-        if(this.cursors.left.isDown) {
-            this.ship.setAngularVelocity(-150);
-        } else if(this.cursors.right.isDown) {
-            this.ship.setAngularVelocity(150);
-        } else {
-            this.ship.setAngularVelocity(0);
-        }
-
-        if(this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-        } else {
-            this.ship.setAcceleration(0);
-        }
-
-        this.physics.world.wrap(this.ship, 5);
-        
-        // emit player movement
-        var x = this.ship.x;
-        var y = this.ship.y;
-        var r = this.ship.rotation;
-        if(this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-            this.socket.emit("playerMovement", {
-                x: this.ship.x,
-                y: this.ship.y,
-                rotation: this.ship.rotation
+// https://micropi.wordpress.com/2020/05/02/making-a-top-down-game-in-phaser-3/
+const Input = (() => {
+    return class Input {
+        constructor(game) {
+            this.game = game;
+            this.primaryInput = game.input.keyboard.createCursorKeys();
+            this.altInput = game.input.keyboard.addKeys({
+                up: 'W',
+                left: 'A',
+                down: 'S',
+                right: 'D'
             });
         }
-        // save old position data
-        this.ship.oldPosition = {
-            x: this.ship.x,
-            y: this.ship.y,
-            rotation: this.ship.rotation
-        };
-    }
-}
 
-function addPlayer(self, playerInfo) {
-    self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, "ship")
-        .setOrigin(0.5, 0.5)
-        .setDisplaySize(53, 40);
-    if(playerInfo.team === "blue") {
-        self.ship.setTint(0x0000ff);
-    } else {
-        self.ship.setTint(0xff0000);
-    }
-    self.ship.setDrag(100);
-    self.ship.setAngularDrag(100);
-    self.ship.setMaxVelocity(200);
-}
+        up() {
+            return this.primaryInput.up.isDown || this.altInput.up.isDown;
+        }
 
-function addOtherPlayer(self, playerInfo) {
-    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, "otherPlayer")
-        .setOrigin(0.5, 0.5)
-        .setDisplaySize(53, 40);
-    if(playerInfo.team === "blue") {
-        otherPlayer.setTint(0x0000ff);
-    } else {
-        otherPlayer.setTint(0xff0000);
+        down() {
+            return this.primaryInput.down.isDown || this.altInput.down.isDown;
+        }
+
+        left() {
+            return this.primaryInput.left.isDown || this.altInput.left.isDown;
+        }
+
+        right() {
+            return this.primaryInput.right.isDown || this.altInput.right.isDown;
+        }
+    };
+})();
+
+const MyPlayer = (() => {
+    const MAX_VELOCITY = 200;
+
+    return class MyPlayer {
+        constructor(game, x, y) {
+            this.game = game;
+            this.obj = game.physics.add.sprite(x, y, "circle").setScale(0.5);
+            this.oldPosition = {
+                x: x,
+                y: y,
+                rotation: 0
+            };
+        }
+
+        stopMoving() {
+            this.obj.setVelocity(0, 0);
+        }
+
+        setVelocityDirection(dirX, dirY) {
+            let angle = Math.atan2(dirY, dirX);
+            let velocityX = MAX_VELOCITY * Math.cos(angle);
+            let velocityY = MAX_VELOCITY * Math.sin(angle);
+            this.obj.setVelocity(velocityX, velocityY);
+        }
+
+        getX() {
+            return this.obj.x;
+        }
+
+        getY() {
+            return this.obj.y;
+        }
+    };
+})();
+const Game = (() => {
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: "arcade",
+            arcade: {
+                debug: false // Turn this on to see hitboxes
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    /* SETUP */
+    const game = new Phaser.Game(config);
+    let inputHandler;
+    let socket;
+
+    function preload() {
+        this.load.image("floor", "assets/floor.png");
+        this.load.image("circle", "assets/circle.png");
+        this.load.image("other", "assets/other.png");
+        this.load.image("obstacle", "assets/obstacle.png");
     }
-    otherPlayer.playerId = playerInfo.playerId;
-    self.otherPlayers.add(otherPlayer);
-}
+
+    function create() {
+        // Setup socket
+        setupSocket(this);
+
+        // Setup background
+        let background = this.add.image(0, 0, "floor")
+            .setOrigin(0, 0); // Center image
+        this.xLimit = background.displayWidth;
+        this.yLimit = background.displayHeight;
+
+        // Setup player
+        this.player = new MyPlayer(this, 300, 300);
+
+        // Setup camera
+        const CAMERA_SPEED = 0.2;
+        this.cameras.main
+            .setBounds(0, 0, this.xLimit, this.yLimit)
+            .startFollow(this.player.obj)
+            .setRoundPixels(false) // default false
+            .setLerp(CAMERA_SPEED, CAMERA_SPEED);
+
+        // Setup obstacles
+        this.obstacles = this.physics.add.staticGroup(); //create group for obstacles
+        this.obstacles.create(800, 600, "obstacle");
+        this.obstacles.create(900, 800, "obstacle");
+
+        this.physics.add.collider(this.player.obj, this.obstacles); //collision detection between player and obstacles
+
+        // Setup input
+        inputHandler = new Input(this);
+    }
+    
+    function setupSocket(gameObj) {
+        socket = io();
+        gameObj.otherPlayers = gameObj.physics.add.group();
+
+        socket.on("currentPlayers", players => {
+            for(id in players) {
+                const otherPlayer = players[id];
+                if(otherPlayer.playerId !== socket.id) {
+                    addOtherPlayer(gameObj, otherPlayer);
+                }
+            }
+        });
+        
+        socket.on("playerConnect", playerInfo => {
+            console.log("playerConnect received");
+            addOtherPlayer(gameObj, playerInfo);
+        });
+        
+        socket.on("playerDisconnect", playerId => {
+            gameObj.otherPlayers.getChildren().forEach(otherPlayer => {
+                if(playerId === otherPlayer.playerId) {
+                    otherPlayer.destroy();
+                }
+            });
+        });
+
+        socket.on("playerMoved", playerInfo => {
+            gameObj.otherPlayers.getChildren().forEach(otherPlayer => {
+                if(playerInfo.playerId === otherPlayer.playerId) {
+                    otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+                    otherPlayer.setRotation(playerInfo.rotation);
+                }
+            });
+        });
+    }
+
+    function addOtherPlayer(gameObj, playerInfo) {
+        const otherPlayer = gameObj.add.sprite(playerInfo.x, playerInfo.y, "other")
+            .setScale(0.5);
+        otherPlayer.playerId = playerInfo.playerId;
+        gameObj.otherPlayers.add(otherPlayer);
+    }
+
+    /* UPDATE */
+    function update() {
+        let dirX = 0;
+        let dirY = 0;
+
+        if(inputHandler.left() && this.player.getX() >= 0) {
+            dirX = -1;
+        }
+        else if(inputHandler.right() && this.player.getX() <= this.xLimit) {
+            dirX = 1;
+        }
+
+        if(inputHandler.up() && this.player.getY() >= 0) {
+            dirY = -1;
+        }
+        else if(inputHandler.down() && this.player.getY() <= this.yLimit) {
+            dirY = 1;
+        }
+
+        if(dirX != 0 || dirY != 0) {
+            this.player.setVelocityDirection(dirX, dirY);
+        } else {
+            this.player.stopMoving();
+        }
+
+        // emit player movement
+        if(playerHasMoved(this.player.obj, this.player.oldPosition)) {
+            let newPos = {
+                x: this.player.obj.x,
+                y: this.player.obj.y,
+                rotation: this.rotation
+            }
+            socket.emit("playerMovement", newPos)
+            this.oldPosition = newPos;
+        }
+    }
+
+    function playerHasMoved(playerObj, oldPosition) {
+        const x = playerObj.x;
+        const y = playerObj.y;
+        const rotation = playerObj.rotation;
+        return (oldPosition.x !== x) || (oldPosition.y !== y) || (oldPosition.rotation !== rotation);
+    }
+
+    return {
+        getGame() {
+            return game;
+        }
+    };
+})();
